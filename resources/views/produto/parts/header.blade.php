b310ed835390f718028f39f7015603c479d95e96
enzo <enzohhamada@gmail.com>
2025-12-04 18:00:17 -0300

    arumando o produto e adicionando a tabela produto

--- resources/views/produto/parts/header.blade.php
+++ resources/views/produto/parts/header.blade.php
@@ -3,3 +3,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title> 
 </header>
+
+<div class="container mt-4">
+    <h3 class="mb-3">Tabela de Exemplo</h3>
+</div>
